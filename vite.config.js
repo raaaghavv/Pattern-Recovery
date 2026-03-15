e95c5a7970bb16b7b82b57ec653a4bbabae85d54
raaaghavv <raghavgoel454@gmail.com>
2026-03-15 18:47:45 +0530

    setup github actions

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Pattern-Recovery/",
   plugins: [react()],
   build: {
     target: "es2020",
